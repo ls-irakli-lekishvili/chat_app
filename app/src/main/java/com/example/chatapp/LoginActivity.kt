aca094d141414f7b9c6b4b20334adbc743628edb
Your Namect git config --global user.email <roma.chikhladze777@gmail.com>
2021-01-28 23:25:30 +0400

    added file photo uload to firebase and save user to db

--- app/src/main/java/com/example/chatapp/LoginActivity.kt
+++ app/src/main/java/com/example/chatapp/LoginActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,15 +17,31 @@ class LoginActivity: AppCompatActivity() {
 
 
         findViewById<Button>(R.id.login_button_login).setOnClickListener {
-            val email = findViewById<EditText>(R.id.email_editText_login).text.toString()
-            val password = findViewById<EditText>(R.id.password_editText_login).text.toString()
-
-            Log.d("Login", "Attempt login with email/pw: $email/***")
-
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-//          .addOnCompleteListener()
-//          .add
-
+            performLogin()
         }
+
+        findViewById<TextView>(R.id.back_to_register_textview).setOnClickListener{
+            finish()
+        }
+    }
+
+    private fun performLogin() {
+        val email = findViewById<EditText>(R.id.email_editText_login).text.toString()
+        val password = findViewById<EditText>(R.id.password_editText_login).text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill out email/pw.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d("Login", "Successfully logged in: ${it.result?.user?.uid}")
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Failed to log in: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
     }
 }
