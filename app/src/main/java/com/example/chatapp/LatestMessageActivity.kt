aceac96c5d6d43388fc93f04fe829dc2f7c41e85
yesntface <i_lekishvili@cu.edu.ge>
2021-01-28 23:26:03 +0400

    added latest message activity and new message activity

---
+++ app/src/main/java/com/example/chatapp/LatestMessageActivity.kt
@@ -0,0 +1,45 @@
+package com.example.chatapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LatestMessageActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        verifyUserIsLoggedIn()
+    }
+
+    private fun verifyUserIsLoggedIn() {
+        val uid = FirebaseAuth.getInstance().uid
+        if (uid == null) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_new_message -> {
+                val intent = Intent(this, NewMessageActivity::class.java)
+                startActivity(intent)
+            }
+
+            R.id.menu_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
